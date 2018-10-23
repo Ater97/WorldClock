992ee13ba78d9a8b184d3cce43c338141851853f
Sebastián Orantes Chang <ater7991@gmail.com>
2018-10-22 19:22:53 -0600

    negative utc missing

--- datetime.asm
+++ datetime.asm
@@ -29,15 +29,17 @@ Op2Examplestr db 13,10,'Example UTC+05 or UTC-10$'
 Op2Helpstr    db 13,10,'UTC$'
 Op2Error      db 13,10,':s Please enter a valid format$'
 ;------------------Option3-------------------------------- 
-India       DB 13,10, 'a. India: $'
-alemania    DB 13,10, 'b. alemania: $'
-EEUU        DB 13,10, 'c. EEUU: $'
-Argentina   DB 13,10, 'd. Argentina: $'
-Japon       DB 13,10, 'e. Japon: $'
-
+Indiastr      DB 13,10, 'a. India: $'
+Alemaniastr   DB 13,10, 'b. alemania: $'
+EEUUstr       DB 13,10, 'c. EEUU: $'
+Argentinastr  DB 13,10, 'd. Argentina: $'
+Japonstr      DB 13,10, 'e. Japon: $'
+Op3Datestr    DB 13,10, 'Date: $'
+Op3Timestr    DB 13,10, 'Time: $'
 ;------------------Option4-------------------------------- 
 Op4Timestr  DB 13,10,'Enter new time: $'
 Op4Datestr  DB 13,10,'Enter new date: $'
+
 ;------------------Option5------------------------------- 
 
 ;---------------------UTILITIES--------------------------
@@ -53,6 +55,7 @@ PRUEBA1    DB 13,10, 'PRUEBA1: $'
 PRUEBA2    DB 13,10, 'PRUEBA2: $'
 num1        db ?
 num2        db ?
+num3        db ?
 rst         db ?
 rst2        db ?
 module      db ?
@@ -165,13 +168,67 @@ Error:
 Option3beta1:
 jmp Option3
 Option4beta1:
-jmp Option4
+jmp Option4beta2
 Option5beta1:
-jmp Option5
+jmp Option5beta2
 ;------------------Option3--------------------------------   
 Option3:
     call    CleanScreen
+    lea BX, date
+    call get__date
+    
+    lea dx,Op1DateStr
+    call PrintStr       ;System date:
+    lea dx,date
+    call PrintStr       ;display date
+    lea dx, Op1TimeStr  ;System time:    
+    call PrintStr
+    call get_time       ;display time
+India: ;UTC+5:30
+    lea     dx,Salto
+    call    PrintStr
+    lea     dx,Indiastr
+    call    PrintStr
+    mov     num1,5
+    mov     num3,30
+    call    utcProcedureIndia
+Alemania: ;UTC+2
+    lea     dx,Salto
+    call    PrintStr
+    lea     dx,Alemaniastr
+    call    PrintStr
+    mov     num1,2
+    call    utcProcedure
+
+USA: ;UTC-5
+    lea     dx,Salto
+    call    PrintStr
+    lea     dx,EEUUstr
+    call    PrintStr
+    mov     num1,-5
+    call    utcProcedure
+
+Argentina: ;UTC-3
+    lea     dx,Salto
+    call    PrintStr
+    lea     dx,Argentinastr
+    call    PrintStr
+    mov     num1,-3
+    call    utcProcedure
+
+Japon: ;UTC+9
+    lea     dx,Salto
+    call    PrintStr
+    lea     dx,Japonstr
+    call    PrintStr
+    mov     num1,9
+    call    utcProcedure
+
     call Continue
+Option4beta2:
+jmp Option4
+Option5beta2:
+jmp Option5
 ;------------------Option4--------------------------------   
 Option4:
     call    CleanScreen
@@ -186,11 +243,11 @@ Option4:
 Option5:
     call    CleanScreen
     call    Continue
-
-
 ;---------------------UTILITIES--------------------------
 utcProcedure proc
-;Hour Part
+    ;Hour Part
+    lea     dx,Op3Timestr
+    call    PrintStr
     mov AH,2CH    ; To get System Time
     int 21H
     mov al,CH     ; Hour is in CH
@@ -203,14 +260,11 @@ utcProcedure proc
     call DISP
     ;Minutes and seconds
     call    get_minutes
-    lea     dx,Salto
+    lea     dx,Op3Datestr
     call    PrintStr
     call    get_date
     ret
 AdjustmentUTC:
-    lea DX,PRUEBA1
-    call PrintStr
-
     call    CleanV ;clean dx and ax
     mov     al,num2
     mov     bl,25
@@ -222,7 +276,7 @@ AdjustmentUTC:
     mov     BX,AX
     call    DISP
     call    get_minutes
-    lea     dx,Salto
+    lea     dx,Op3Datestr
     call    PrintStr
     ;Day Part
     mov     AH,2AH    ; To get System Date
@@ -271,95 +325,139 @@ AdjustmentUTC:
 
 ret
 endp
+utcProcedureIndia proc
+    ;Hour Part
+    call    CleanV ;clean dx and ax
+    lea     dx,Op3Timestr
+    call    PrintStr
+    mov AH,2CH    ; To get System Time
+    int 21H
+    mov al,CL     ; Minutes is in CL
+    add al,num3
+    mov bl,61
+    div bl
+    mov rst,al      ;add to hours
+    mov MINUTE,ah   ;new minutes
 
-getMonthDays proc
-    cmp Month,1
-    je  Month1
-    cmp Month,2
-    je  Month2
-    cmp Month,3
-    je  Month3
-    cmp Month,4
-    je  Month4
-    cmp Month,5
-    je  Month5
-    cmp Month,6
-    je  Month6
-    cmp Month,7
-    je  Month7
-    cmp Month,8
-    je  Month8
-    cmp Month,8
-    je  Month8
-    cmp Month,9
-    je  Month9
-    cmp Month,10
-    je  Month10
-    cmp Month,11
-    je  Month11
-    cmp Month,12
-    je  Month12
-    Month1:
-        mov DAY,31
-        ret
-    Month2:
-        mov day,28
-        ret
-    Month3:
-        mov DAY,31
-        ret
-    Month4:
-        mov DAY,30
-        ret
-    Month5:
-        mov DAY,31
-        ret
-    Month6:
-        mov DAY,30
-        ret
-    Month7:
-        mov DAY,31
-        ret
-    Month8:
-        mov DAY,31
-        ret
-    Month9:
-        mov DAY,30
-        ret
-    Month10:
-        mov DAY,31
-        ret
-    Month11:
-        mov DAY,30
-        ret
-    Month12:
-        mov DAY,31
-        ret
-    endp
+    mov al,CH     ; Hour is in CH
+    add al,num1   
+    add al, rst
+    mov num2,al
+    cmp num2,24
+    jg  AdjustmentUTCIndia ;if is necesary to change date
+    AAM
+    mov BX,AX
+    call DISP
+    ;Minutes and seconds
+    mov dl,':'
+    mov AH,02H    ; To Print : in DOS
+    int 21H
+    ;Minutes Part
+    mov al,MINUTE     ; Minutes is in CL
+    AAM
+    mov BX,AX
+    call DISP
+    mov dl,':'    ; To Print : in DOS
+    mov AH,02H
+    int 21H
+    ;Seconds Part
+    mov AH,2CH    ; To get System Time
+    int 21H
+    mov al,DH     ; Seconds is in DH
+    AAM
+    mov BX,AX
+    call DISP
 
-printNum1 proc
-    mov al, num1
-    mov BL, 10 
-    DIV BL  
+    ;
+    lea     dx,Op3Datestr
+    call    PrintStr
+    call    get_date
+    ret
+AdjustmentUTCIndia:
+    call    CleanV ;clean dx and ax
+    mov     al,num2
+    mov     bl,25
+    div     bl
+    mov     rst,al      ; add rst to day
+    mov     hour,ah   ;new hour
+    mov     al,hour     ; Hour is in module
+    AAM
+    mov     BX,AX
+    call    DISP
     
-    mov rst,al
-    mov rst2,AH
-    XOR DX,DX;clean
+    mov dl,':'
+    mov AH,02H    ; To Print : in DOS
+    int 21H
 
-    ;print result
-    mov AH,02h
-    mov dl,rst
-    ADD dl,30h
-    int 21h
-    
-    XOR DX,DX ;clean
-    mov AH,02h
-    mov dl,rst2
-    ADD dl,30h
-    int 21h
+    ;Minutes Part
+    mov AH,2CH    ; To get System Time
+    int 21H
+    mov al,CL     ; Minutes is in CL
+    AAM
+    mov BX,AX
+    call DISP
+
+    mov dl,':'    ; To Print : in DOS
+    mov AH,02H
+    int 21H
+
+    ;Seconds Part
+    mov AH,2CH    ; To get System Time
+    int 21H
+    mov al,DH     ; Seconds is in DH
+    AAM
+    mov BX,AX
+    call DISP
+
+    lea     dx,Op3Datestr
+    call    PrintStr
+    ;Day Part
+    mov     AH,2AH    ; To get System Date
+    int     21H
+    mov     al,dl     ; Day is in dl
+    add     al,rst
+    mov num2,al     ;days 
+    mov MONTH,dh  ; Month is in DH
+    mov YEAR,cx     ;   (1980 through 2099)
+    call getMonthDays ; DAY have the amount of days of the actual month
+    call CleanV
+
+    mov al,num2
+    mov bl,Day
+    div bl
+    mov rst,al  ;add rsr to month
+    mov module,ah; new day
+    mov al,module   ;day is in module
+    AAM
+    mov BX,AX
+    call DISP
+    mov dl,'/'
+    mov AH,02H    ; To Print / in DOS
+    int 21H
+    call CleanV
+    ;Month Part
+    mov al,month   
+    add al,rst
+
+    AAM
+    mov BX,AX
+    call DISP
+
+    mov dl,'/'    ; To Print / in DOS
+    mov AH,02H
+    int 21H
+
+    ;Year Part (1980 through 2099)
+    mov AH,2AH    ; To get System Date
+    int 21H
+    ADD CX,0F830H ; To negate the effects of 16bit value,
+    mov AX,CX     ; since AAM is applicable only for al (YYYY -> YY)
+    AAM
+    mov BX,AX
+    call DISP
+
 ret
 endp
-
 
 set_time proc ;Regresa CH = hora, CL = minutos, DH = segundos y dl = centésimos de segundo.
     mov ah,2d
@@ -507,112 +605,198 @@ convert proc
     pop dx
     ret 
     endp
-;-----------------------------------------------------
-ReadTwoint  proc
-    call    Readint
-    mov     num1,al
-    call    Readint
-    mov     num2,al
-    mov al,10
-    mul num1    ;a base 10
-    add al,num2  
-    mov num1, al 
-    ret
+getMonthDays proc
+    cmp Month,1
+    je  Month1
+    cmp Month,2
+    je  Month2
+    cmp Month,3
+    je  Month3
+    cmp Month,4
+    je  Month4
+    cmp Month,5
+    je  Month5
+    cmp Month,6
+    je  Month6
+    cmp Month,7
+    je  Month7
+    cmp Month,8
+    je  Month8
+    cmp Month,8
+    je  Month8
+    cmp Month,9
+    je  Month9
+    cmp Month,10
+    je  Month10
+    cmp Month,11
+    je  Month11
+    cmp Month,12
+    je  Month12
+    Month1:
+        mov DAY,31
+        ret
+    Month2:
+        mov day,28
+        ret
+    Month3:
+        mov DAY,31
+        ret
+    Month4:
+        mov DAY,30
+        ret
+    Month5:
+        mov DAY,31
+        ret
+    Month6:
+        mov DAY,30
+        ret
+    Month7:
+        mov DAY,31
+        ret
+    Month8:
+        mov DAY,31
+        ret
+    Month9:
+        mov DAY,30
+        ret
+    Month10:
+        mov DAY,31
+        ret
+    Month11:
+        mov DAY,30
+        ret
+    Month12:
+        mov DAY,31
+        ret
     endp
+printNum1 proc
+    mov al, num1
+    mov BL, 10 
+    DIV BL  
+    
+    mov rst,al
+    mov rst2,AH
+    XOR DX,DX;clean
 
-PrintWeekDay proc
-    cmp WEEKDAY,1
-    je  mon
-    cmp WEEKDAY,2
-    je  tue
-    cmp WEEKDAY,3
-    je  wed
-    cmp WEEKDAY,4
-    je  thu
-    cmp WEEKDAY,5
-    je  fri
-    cmp WEEKDAY,6
-    je  sat
-    cmp WEEKDAY,7
-    je  sun
-    mon:
-        lea dx,Monday
-        call PrintStr
-        ret
-    tue:
-        lea dx,Tuesday
-        call PrintStr
-        ret
-    wed:
-        lea dx,Wednesday
-        call PrintStr
-        ret
-    thu:
-        lea dx,Thusday
-        call PrintStr
-        ret
-    fri:
-        lea dx,Friday
-        call PrintStr
-        ret
-    sat:
-        lea dx,Saturday
-        call PrintStr
-        ret
-    sun:
-        lea dx,Sunday
-        call PrintStr
-        ret
-        endp
-
-Continue proc
-    lea     DX,AskContinue
-    call    PrintStr
-    waitl:          
-        mov ah, 0bh;check for a key 
-        int 21h
-        cmp al,0
-        je waitl
-        xor ax,ax
-        mov AH, 01h  
-        int 21h
-        call CleanScreen
-        jmp Menu
-        endp
-CleanScreen proc
-    mov al, 00
-    mov ah, 00
-    int 10h
-    ret
-    endp
-Printint proc
-    mov AH,02h ;print dl
+    ;print result
+    mov AH,02h
+    mov dl,rst
+    ADD dl,30h
+    int 21h
+    
+    XOR DX,DX ;clean
+    mov AH,02h
+    mov dl,rst2
     ADD dl,30h
     int 21h
     ret
     endp
-PrintStr proc 
-    mov AH, 09h ;PrintStr DX
-    int 21h
-    ret
-    endp
-ReadChar proc
-    mov AH , 01h
-    int 21h
-    ret
-    endp
-Readint proc
-    XOR al,al
-    mov AH, 01h  
-    int 21h 
-    SUB al,30h 
-    ret
-    endp
-CleanV proc
-    xor dx,dx
-    xor ax,ax
-    ret
-    endp
+;-------------Common stuff----------------------
+    ReadTwoint  proc
+        call    Readint
+        mov     num1,al
+        call    Readint
+        mov     num2,al
+        mov al,10
+        mul num1    ;a base 10
+        add al,num2  
+        mov num1, al 
+        ret
+        endp
+
+    PrintWeekDay proc
+        cmp WEEKDAY,1
+        je  mon
+        cmp WEEKDAY,2
+        je  tue
+        cmp WEEKDAY,3
+        je  wed
+        cmp WEEKDAY,4
+        je  thu
+        cmp WEEKDAY,5
+        je  fri
+        cmp WEEKDAY,6
+        je  sat
+        cmp WEEKDAY,7
+        je  sun
+        mon:
+            lea dx,Monday
+            call PrintStr
+            ret
+        tue:
+            lea dx,Tuesday
+            call PrintStr
+            ret
+        wed:
+            lea dx,Wednesday
+            call PrintStr
+            ret
+        thu:
+            lea dx,Thusday
+            call PrintStr
+            ret
+        fri:
+            lea dx,Friday
+            call PrintStr
+            ret
+        sat:
+            lea dx,Saturday
+            call PrintStr
+            ret
+        sun:
+            lea dx,Sunday
+            call PrintStr
+            ret
+            endp
+
+    Continue proc
+        lea     DX,AskContinue
+        call    PrintStr
+        waitl:          
+            mov ah, 0bh;check for a key 
+            int 21h
+            cmp al,0
+            je waitl
+            xor ax,ax
+            mov AH, 01h  
+            int 21h
+            call CleanScreen
+            jmp Menu
+            endp
+    CleanScreen proc
+        mov al, 00
+        mov ah, 00
+        int 10h
+        ret
+        endp
+    Printint proc
+        mov AH,02h ;print dl
+        ADD dl,30h
+        int 21h
+        ret
+        endp
+    PrintStr proc 
+        mov AH, 09h ;PrintStr DX
+        int 21h
+        ret
+        endp
+    ReadChar proc
+        mov AH , 01h
+        int 21h
+        ret
+        endp
+    Readint proc
+        XOR al,al
+        mov AH, 01h  
+        int 21h 
+        SUB al,30h 
+        ret
+        endp
+    CleanV proc
+        xor dx,dx
+        xor ax,ax
+        ret
+        endp
 
 ;---------------------------------------------------------------
 .STACK
